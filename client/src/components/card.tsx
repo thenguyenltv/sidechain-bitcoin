1f481ceee8de0c842eef5c1a5a1bcbbd8f1c372b
Dustin.T <dustin.t@itcgroup.io>
2024-06-19 06:53:54 +0700

    Update modal UI

--- client/src/components/card.tsx
+++ client/src/components/card.tsx
@@ -12,10 +12,20 @@ import Web3 from "web3";
 
 import { useGlobalState } from "@/context/GlobalStateContext";
 import { useState, useEffect } from "react";
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 
 export default function card() {
   const imageSrc = "";
   const subImageSrc = "";
+  const [amount, setAmount] = useState(0);
+  const [open, setOpen] = useState(false);
   const {
     wallet1,
     setWallet1,
@@ -89,7 +99,9 @@ export default function card() {
 
   async function handleExchangeClick() {
     if (address) {
-      alert("Exchange");
+      if (from.currency && to.currency && amount > 0) {
+        setOpen(true);
+      }
     } else {
       if (window.ethereum) {
         try {
@@ -112,6 +124,16 @@ export default function card() {
       }
     }
   }
+
+  const handleAmountChange = (event) => {
+    const value = event.target.value;
+
+    if (value === "" || Number(value) === 0) {
+      setAmount(0);
+    } else {
+      setAmount(Number(value));
+    }
+  };
 
   return (
     <>
@@ -172,17 +194,18 @@ export default function card() {
                 </div>
               </div>
               <div className="info__text">
-                <input type="number" placeholder="0" />
-                <p className="rate">$0.00</p>
+                <input
+                  type="number"
+                  placeholder="0"
+                  onChange={handleAmountChange}
+                />
+                <p className="rate">${(amount * 70000).toFixed(2)}</p>
               </div>
             </div>
           </div>
         </div>
 
-        <button
-          className="button-type2 w-full "
-          onClick = {handleExchangeClick}
-        >
+        <button className="button-type2 w-full " onClick={handleExchangeClick}>
           {address ? "Exchange" : "Connect wallet"}
         </button>
       </div>
@@ -228,7 +251,7 @@ export default function card() {
             >
               {coin.image && <img src={coin.image.src} alt={coin.name} />}
             </div>
-            <div className="info-text absolute bottom-full mb-2 text-xs bg-gray-800 text-white p-1 rounded opacity-0 transition-opacity duration-300">
+            <div className="info-text absolute bottom-full mb-2 text-xs bg-gray-800 text-white p-1 rounded-2xl opacity-0 transition-opacity duration-300">
               {coin.name}
             </div>
           </div>
@@ -240,11 +263,93 @@ export default function card() {
           >
             <FontAwesomeIcon icon={faXmark} />
           </div>
-          <div className="info-text absolute bottom-full mb-2 text-xs bg-gray-800 text-white p-1 rounded opacity-0 transition-opacity duration-300">
+          <div className="info-text absolute bottom-full mb-2 text-xs bg-gray-800 text-white p-1 rounded-2xl opacity-0 transition-opacity duration-300">
             Remove
           </div>
         </div>
       </div>
+
+      <Transition show={open}>
+        <Dialog className="relative z-50" onClose={setOpen}>
+          <TransitionChild
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </TransitionChild>
+
+          <div className="fixed inset-0 z-50 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <TransitionChild
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <DialogPanel className="relative transform overflow-hidden rounded-2xl bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg modal-deposit">
+                  <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start justify-center">
+                      <div className="mt-3 text-center sm:mt-0">
+                        <DialogTitle
+                          as="h3"
+                          className="text-xl font-bold leading-6 text-gray-900 text-center"
+                        >
+                          Send deposit
+                        </DialogTitle>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="info bg-white px-4 pb-4 pt-8 sm:p-6 sm:pb-4">
+                    <div className="amount flex justify-between mb-3">
+                      <p className="text-sm font-semibold leading-6 text-gray-600 ">
+                        Amount
+                      </p>
+                      <p className="text-base	 font-semibold leading-6 text-gray-900">
+                        {amount} BTC
+                      </p>
+                    </div>
+                    <div className="fee flex justify-between mb-3">
+                      <p className="text-sm font-semibold leading-6 text-gray-600">
+                        Fee
+                      </p>
+                      <p className="text-base	 font-semibold leading-6 text-gray-900">
+                        -0.0001 BTC
+                      </p>
+                    </div>
+                    <div className="lock flex justify-between pb-8 border-b">
+                      <p className="text-sm font-semibold leading-6 text-gray-600">
+                        Lock period
+                      </p>
+                      <p className="text-base	 font-semibold leading-6 text-gray-900">
+                        9 months
+                      </p>
+                    </div>
+                  </div>
+                  <div className="notify bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <p className="text-xs font-semibold leading-6 text-red-600 mb-4">
+                      Sent over {amount} BTC to your wallet to above address.
+                    </p>
+                    <p className="text-xs leading-6 text-gray-900">
+                      Your Deposit and HODL Score will be linked to{" "}
+                      {address.slice(0, 6) + "..." + address.slice(-4)}.
+                    </p>
+                    <p className="text-xs leading-6 text-gray-900">
+                      This address will be able to withdraw funds and extend
+                      lock duration.
+                    </p>
+                  </div>
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </>
   );
 }
